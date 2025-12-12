e710bb18f7739c3d85111b93246afeb6450bb94e
Jerrod Sunderland <jerrod.sunderland@gmail.com>
2025-12-11 19:16:16 -0500

    Update GetAllAsyncTests.cs

--- TemplateMongo.Tests/Dao/BasicDaoTests/GetAllAsyncTests.cs
+++ TemplateMongo.Tests/Dao/BasicDaoTests/GetAllAsyncTests.cs
@@ -1,33 +1,34 @@
-// using MongoDB.Driver;
-// using Moq;
-// using TemplateMongo.Models;
+using MongoDB.Driver;
+using Moq;
+using TemplateMongo.Models;
+using TemplateMongo.Parameters;
 
-// namespace TemplateMongo.Tests.Dao.BasicDaoTests;
+namespace TemplateMongo.Tests.Dao.BasicDaoTests;
 
-// public class GetAllAsyncTests : BasicDaoTestsBase
-// {
-//     [Fact]
-//     public async Task GetAllAsync_ReturnsAllModels()
-//     {
-//         var models = new List<BasicModel>
-//         {
-//             new BasicModel { Id = "1", Name = "A" },
-//             new BasicModel { Id = "2", Name = "B" }
-//         };
+public class GetAllAsyncTests : BasicDaoTestsBase
+{
+    [Fact]
+    public async Task GetAllAsync_ReturnsAllModels()
+    {
+        var models = new List<BasicModel>
+        {
+            new BasicModel { Id = "1", Name = "A" },
+            new BasicModel { Id = "2", Name = "B" }
+        };
 
-//         var mockCursor = CreateMockCursor(models);
+        var mockCursor = CreateMockCursor(models);
 
-//         _mockCollection
-//             .Setup(c => c.FindAsync(
-//                 It.IsAny<FilterDefinition<BasicModel>>(),
-//                 It.IsAny<FindOptions<BasicModel, BasicModel>>(),
-//                 It.IsAny<CancellationToken>()))
-//             .ReturnsAsync(mockCursor.Object);
+        _mockCollection
+            .Setup(c => c.FindAsync(
+                It.IsAny<FilterDefinition<BasicModel>>(),
+                It.IsAny<FindOptions<BasicModel, BasicModel>>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(mockCursor.Object);
 
-//         var result = await _dao.GetAllAsync();
+        var result = await _dao.GetAllAsync(new GetAllBasicParams() {});
 
-//         Assert.Equal(2, result.Count);
-//         Assert.Equal("1", result[0].Id);
-//         Assert.Equal("2", result[1].Id);
-//     }
-// }
+        Assert.Equal(2, result.Items.Count);
+        Assert.Equal("1", result.Items[0].Id);
+        Assert.Equal("2", result.Items[1].Id);
+    }
+}
