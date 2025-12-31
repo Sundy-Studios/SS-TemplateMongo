5409afe73ff96f5e396faea73e26f65adb793eb4
Jerrod Sunderland <jerrod.sunderland@gmail.com>
2025-12-31 00:57:46 -0500

    fix tests

--- TemplateMongo.Tests/Services/Internal/InternalBasicServiceTests/DeleteAsyncTests.cs
+++ TemplateMongo.Tests/Services/Internal/InternalBasicServiceTests/DeleteAsyncTests.cs
@@ -1,5 +1,6 @@
 namespace TemplateMongo.Tests.Services.InternalBasicServiceTests;
 
+using Common.Exception.Models;
 using Moq;
 
 public class DeleteAsyncTests : InternalBasicServiceTestsBase
@@ -9,8 +10,47 @@ public class DeleteAsyncTests : InternalBasicServiceTestsBase
     {
         var id = "1";
 
+        MockUser
+        .Setup(u => u.IsAuthenticated)
+        .Returns(true);
+
         await Service.DeleteAsync(id);
 
         MockDomain.Verify(d => d.DeleteAsync(id, It.IsAny<CancellationToken>()), Times.Once);
     }
+
+    [Fact]
+    public async Task DeleteAsyncFailsAuthenticationIsFalse()
+    {
+        var id = "1";
+
+        MockUser
+        .Setup(u => u.IsAuthenticated)
+        .Returns(false);
+
+        var ex = await Assert.ThrowsAsync<ForbiddenException>(
+            () => Service.DeleteAsync(id)
+        );
+
+        Assert.Equal(
+            "User is not authorized to delete items.",
+            ex.Message
+        );
+    }
+
+    [Fact]
+    public async Task DeleteAsyncFailsAuthenticationIsNull()
+    {
+        var id = "1";
+
+        var ex = await Assert.ThrowsAsync<ForbiddenException>(
+            () => Service.DeleteAsync(id)
+        );
+
+        Assert.Equal(
+            "User is not authorized to delete items.",
+            ex.Message
+        );
+    }
+
 }
